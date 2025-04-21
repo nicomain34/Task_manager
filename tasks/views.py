f082475a111af4df336873aa4f723b6565e5370e
Dmytro <nicomain34@gmail.com>
2025-04-21 12:00:31 +0200

    перший коміт

--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
-from django.shortcuts import get_object_or_404, redirect
 from .forms import TaskForm
 
 def task_list(request):
-    sort = request.GET.get('sort', 'created')
-
-    if sort == 'deadline':
-        tasks = Task.objects.order_by('deadline')  # дедлайн ближчий — вище
-    elif sort == 'created':
-        tasks = Task.objects.order_by('-created_at')  # нові задачі — зверху
+    # Фільтрація
+    filter_option = request.GET.get('filter', 'all')
+    
+    if filter_option == 'completed':
+        tasks = Task.objects.filter(completed=True)
+    elif filter_option == 'incomplete':
+        tasks = Task.objects.filter(completed=False)
     else:
         tasks = Task.objects.all()
 
+    # Сортування
+    sort_option = request.GET.get('sort', 'created')
+    if sort_option == 'deadline':
+        tasks = tasks.order_by('deadline')
+    elif sort_option == 'completed':
+        tasks = tasks.order_by('completed')  # невиконані будуть вгорі
+    else:
+        tasks = tasks.order_by('-created_at')
+
+    # Обробка форми
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -21,14 +31,16 @@ def task_list(request):
     else:
         form = TaskForm()
 
-    return render(request, 'tasks/task_list.html', {
+    context = {
         'tasks': tasks,
-        'form': form
-    })
+        'form': form,
+        'filter_option': filter_option
+    }
+    return render(request, 'tasks/task_list.html', context)
+
 
 def toggle_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.completed = not task.completed
     task.save()
     return redirect('task_list')
-
